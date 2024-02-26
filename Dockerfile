5e97a6f05aa1102a628faa6ca0ed9c803a7a4f4d
tdev <ktgz14211771329@gmail.com>
2024-02-26 20:00:19 +0700

    edit

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,16 @@ COPY ./package.json .
 
 RUN npm install 
 
+RUN npm i nodemon
+
+COPY prisma ./prisma
+
+RUN npx prisma generate
+
 COPY . .
 
 RUN npx prisma migrate 
+
+EXPOSE 5050
 
 CMD ["npm" , "start"]
--- server.ts
+++ server.ts
@@ -29,6 +29,13 @@ class App {
 const port: number = parseInt(process.env.PORT ?? "5050");
 const app = new App().app;
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.listen(port, () => {
   console.log("Server Running on port : " + port);
 });
